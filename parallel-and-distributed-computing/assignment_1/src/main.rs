dbb76d73f9ce1b43c236a0b662168d0b836a6779
robert-birliga <alexandru.barliga@golfgenius.com>
2021-10-18 10:22:42 +0300

    WIP

--- parallel-and-distributed-computing/assignment_1/src/main.rs
+++ parallel-and-distributed-computing/assignment_1/src/main.rs
@@ -1,3 +1,27 @@
+use std::thread;
+
+struct Log {
+    id: u64,
+    text: String
+}
+
+struct Account {
+    balance: u64,
+    logs: Vec<Log>
+}
+
+fn perform_transaction(source_account: u64, destination_account: u64, sum: u64) {
+
+}
+
 fn main() {
+    println!("Hello 🦀s! ");  
     
+    let next_log_id = 1;
+    const NUMBER_OF_THREADS = 10; 
+
+    for i in 
+    thread_handle = thread::spawn(move || {
+        
+    })
 }
