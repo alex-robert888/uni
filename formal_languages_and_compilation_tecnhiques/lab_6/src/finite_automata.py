9402b4dde16e3601218ec5733c444279e2418493
robert-birliga <alexandru.barliga@golfgenius.com>
2021-11-05 18:12:36 +0200

    representatin

--- formal_languages_and_compilation_tecnhiques/lab_6/src/finite_automata.py
+++ formal_languages_and_compilation_tecnhiques/lab_6/src/finite_automata.py
@@ -1,28 +1,48 @@
-class FiniteAutomata(object):
-    def __init__(self, file_path):
-        self.file_path = file_path
+class Transition:
+    def __init__(self, source, destination, input):
+        self.source = source
+        self.destination = destination
+        self.input = input
 
-    def load_sizes(self, f):
-        line = f.readline().split()
-        self.
+    def __str__(self):
+        return f'source: {self.source} | destination: {self.destination} | input: {self.input}'
 
 
-    def load_states(self, f):
-        pass
-
-    def load_initial_state(self, f):
-        pass
-
-    def load_final_states(self, f):
-        pass
+class FiniteAutomata:
+    def __init__(self):
+        self.states = list()
+        self.alphabet = list()
+        self.initial_state = list()
+        self.final_states = list()
+        self.transitions = dict()
 
     def load(self):
-        f = open("finite_automata_1.txt", "r")
-        self.load_sizes(f)
+        f = open("./resources/FA.in", "r")
         self.load_states(f)
+        self.load_alphabet(f)
         self.load_initial_state(f)
         self.load_final_states(f)
+        self.load_transitions(f)
 
+    def load_states(self, f):
+        self.states = f.readline().split()
 
+    def load_alphabet(self, f):
+        self.alphabet = f.readline().split()
 
+    def load_initial_state(self, f):
+        self.initial_state = f.readline().split()[0]
 
+    def load_final_states(self, f):
+        self.final_states = f.readline().split()
+
+    def load_transitions(self, f):
+        while transition := f.readline().split():
+            source = transition[0]
+            destination = transition[1]
+            input = transition[2]
+
+            if input not in self.transitions:
+                self.transitions[input] = [(source, destination)]
+            else:
+                self.transitions[input].append((source, destination))
--- formal_languages_and_compilation_tecnhiques/lab_6/src/main.py
+++ formal_languages_and_compilation_tecnhiques/lab_6/src/main.py
@@ -1,5 +1,11 @@
+from finite_automata import FiniteAutomata
+
+finite_automata = FiniteAutomata()
+
+
 def print_menu():
     print("========= MENU =========")
+    print("0. Initial state.")
     print("1. Set of states.")
     print("2. Alphabet.")
     print("3. Set of transitions.")
@@ -8,7 +14,12 @@ def print_menu():
     print("========================")
 
 
+def is_sequence_accepted(sequence):
+    pass
+
+
 if __name__ == '__main__':
+    finite_automata.load()
 
     while True:
         print_menu()
@@ -16,19 +27,26 @@ if __name__ == '__main__':
         option = 0
         try:
             option = int(input("Enter option: "))
-            assert 1 <= option <= 5
+            assert 0 <= option <= 5
         except:
             print("Invalid option!")
 
-        if option == 1:
-            pass
+        if option == 0:
+            print(finite_automata.initial_state)
+        elif option == 1:
+            print(finite_automata.states)
         elif option == 2:
-            pass
+            print(finite_automata.alphabet)
         elif option == 3:
-            pass
+            print(finite_automata.transitions)
         elif option == 4:
-            pass
+            print(finite_automata.final_states)
         elif option == 5:
-            pass
+            sequence = input("Enter sequence")
+            if is_sequence_accepted(sequence):
+                print("Sequence NOT accepted")
+            else:
+                print("Sequence accepted.")
+
 
 
